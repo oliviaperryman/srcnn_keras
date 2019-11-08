d271493d217fe6066b0049d768d19f712897a2bb
Olivia Perryman <oliviap.soccer@gmail.com>
2019-11-08 16:33:43 -0500

    make default config runnable

--- test.py
+++ test.py
@@ -11,6 +11,7 @@ parser.add_argument('--label_size', type=int, default=None)
 parser.add_argument('--c_dim', type=int, default=1)
 parser.add_argument('--scale', type=int, default=3)
 
+
 def main(args):
     srcnn = SRCNN(
         image_size=args.image_size,
@@ -19,16 +20,22 @@ def main(args):
     X_pre_test, X_test, Y_test = load_test(scale=args.scale)
     predicted_list = []
     for img in X_test:
-        predicted = srcnn.process(img.reshape(1,img.shape[0],img.shape[1],1))
-        predicted_list.append(predicted.reshape(predicted.shape[1],predicted.shape[2],1))
+        img = (img.reshape(
+            1, img.shape[0], img.shape[1], 1)).astype(float)
+        predicted = srcnn.process(img)
+        predicted_list.append(predicted.reshape(
+            predicted.shape[1], predicted.shape[2], 1))
     n_img = len(predicted_list)
-    dirname = './result'
+    dirname = './result2'
     for i in range(n_img):
         imgname = 'image{:02}'.format(i)
-        cv2.imwrite(os.path.join(dirname,imgname+'_original.bmp'), X_pre_test[i])
-        cv2.imwrite(os.path.join(dirname,imgname+'_input.bmp'), X_test[i])
-        cv2.imwrite(os.path.join(dirname,imgname+'_answer.bmp'), Y_test[i])
-        cv2.imwrite(os.path.join(dirname,imgname+'_predicted.bmp'), predicted_list[i])
+        cv2.imwrite(os.path.join(dirname, imgname +
+                                 '_original.bmp'), X_pre_test[i])
+        cv2.imwrite(os.path.join(dirname, imgname+'_input.bmp'), X_test[i])
+        cv2.imwrite(os.path.join(dirname, imgname+'_answer.bmp'), Y_test[i])
+        cv2.imwrite(os.path.join(dirname, imgname +
+                                 '_predicted.bmp'), predicted_list[i])
+
 
 if __name__ == '__main__':
     main(args=parser.parse_args())
